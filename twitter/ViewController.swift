e40dd6240396ae41d54d5319acf2b6206c63a828
toph <kris@shopular.com>
2015-05-24 12:32:26 -0700

    Initial commit from local

---
+++ twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter
+//
+//  Created by Kris Aldenderfer on 5/24/15.
+//  Copyright (c) 2015 Shopular. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
